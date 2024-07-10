0101118e63636110b37bc68008aa5adf1c09f2fb
AngelRionCervi <a.rioncervi@gmail.com>
2024-07-10 15:56:09 +0200

    small fix

--- src/lib/api/controllerApi.ts
+++ src/lib/api/controllerApi.ts
@@ -1,31 +1,4 @@
 import { API_ROUTE } from '$lib/constants';
-import { sleep } from '$lib/helpers/utils';
-
-async function restartController(): Promise<void> {
-	try {
-		const response = await fetch(`${API_ROUTE}/restart`);
-
-		for (;;) {
-			await sleep(1000);
-			try {
-				const pingResponse = await fetch(`${API_ROUTE}/ping`);
-
-				if (pingResponse.ok) {
-					console.log(pingResponse);
-					break;
-				}
-			} catch {
-				// ignore
-			}
-		}
-
-		if (!response.ok) {
-			console.error('Failed to restart controller');
-		}
-	} catch (error) {
-		console.error('An error occurred while restarting the controller:', error);
-	}
-}
 
 async function API_restartController() {
 	try {
