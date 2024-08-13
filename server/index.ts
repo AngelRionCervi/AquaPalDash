c57adbb8f3f7c0a7e21f73a6212fe7aee0378688
AngelRionCervi <a.rioncervi@gmail.com>
2024-08-13 21:32:13 +0200

    tsx again 2

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,14 @@
 import { configureServer } from './wsServer/wsServer';
-import express from 'express'
-import { createServer } from 'http'
-import { handler } from '../build/handler.js'
+import express from 'express';
+import { createServer } from 'http';
+import { handler } from '../build/handler.js';
 
-const port = 3000
-const app = express()
-const server = createServer(app)
+const port = 3001;
+const app = express();
+const server = createServer(app);
 
 configureServer(server);
 
-app.use(handler)
+app.use(handler);
 
-server.listen(port)
+server.listen(port);
