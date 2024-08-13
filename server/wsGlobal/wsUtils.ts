bfd4a52a3cad71cbc1d425c76b3ad8c248dd84b7
AngelRionCervi <a.rioncervi@gmail.com>
2024-08-13 21:10:40 +0200

    copied ws globals

---
+++ server/wsGlobal/wsUtils.ts
@@ -0,0 +1,11 @@
+export function parseMessage(data: string) {
+	try {
+		return JSON.parse(data);
+	} catch (err) {
+		console.error(`Couldn't parse message ${err}`);
+	}
+}
+
+export function jstr(data: object) {
+  return JSON.stringify(data);
+}
