cf42d2e41aaeebf940a179e4d32cde6f97360b61
AngelRionCervi <a.rioncervi@gmail.com>
2024-08-13 21:23:47 +0200

    trying port on dash

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import { configureServer } from './wsServer/wsServer';
 import express from 'express'
 import { createServer } from 'http'
-
 import { handler } from '../build/handler.js'
 
 const port = 3000
